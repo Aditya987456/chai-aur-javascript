f4f178958f5973373c807551d89ebcbb6a0c630b
ADITYA RAJ <132116417+Aditya987456@users.noreply.github.com>
2024-09-06 18:34:38 +0000

    container config done

---
+++ basic1/variables.js
@@ -0,0 +1,17 @@
+const ac_Id = 844126  //fixed hi rahega
+let ac_Email = "aman@google.com"
+var ac_Password = "12345"   //try to not use this
+ac_City = "patna" 
+let ac_State;
+
+// ac_tId = 2 // not allowed becz const ko change nahi kar sakte hai
+
+
+ac_Password = "74555144551"
+
+
+console.log(ac_Id); // isse ek ek ayega o/p
+
+
+
+console.table([ac_Id, ac_Email, ac_Password, ac_City, ac_State])  //ek baar me sab o/p but in table....
