d8b6da227180adf9b1267a36db6f84fb95029c0d
ADITYA RAJ <132116417+Aditya987456@users.noreply.github.com>
2024-09-07 11:31:16 +0000

    data types

---
+++ basic1/datatypes.js
@@ -0,0 +1,29 @@
+"use strict"
+
+let score=35
+
+// let bigInt1=14725836996385244444444444;
+
+let biiig = 12345678901234567890123456777777777777777777777777n  //n lagana hoga last me.
+//console.log(typeof biiig)
+
+let ha_ki_na=true
+console.log(typeof ha_ki_na)
+
+let sent="buttan sarkar"
+console.log(typeof sent)
+
+let khali=null
+console.log(typeof khali)   //  IMP --->  for interview
+
+
+let notdef
+console.log(typeof notdef)
+
+
+
+
+
+
+
+
