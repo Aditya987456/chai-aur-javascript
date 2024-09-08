89eae0994944fd200678ae9298cb39ad1146b4ed
ADITYA RAJ <132116417+Aditya987456@users.noreply.github.com>
2024-09-08 17:19:59 +0000

    conversions

---
+++ basic1/conversions.js
@@ -0,0 +1,100 @@
+
+/*************           STRING -----> NUM,BOOL,Null, Undefined.            ***********/
+
+// let str="ab23"
+// console.log(typeof str)
+
+// //number
+// let score=Number(str)
+// console.log(typeof score)
+// console.log(score)
+
+//boolean
+// let bool=Boolean(str)
+// console.log(typeof bool)
+// console.log(bool)
+
+//null   ---------->     TypeError: null is not a function
+// let nu=null(str)
+// console.log(typeof nu)
+// console.log(nu)
+
+//undefined  -------------->   TypeError: undefined is not a function
+// let unde=undefined(str)
+// console.log(typeof unde)
+// console.log(unde)
+
+
+/******************        NUMBER------>  string,bool,null,undefined *************************/
+
+// let num=0
+// console.log(typeof num)
+
+// //boolean
+// let boool=Boolean(num)
+// console.log(boool)
+
+//string
+// let str=String(num)
+// console.log(str)
+
+//null  ---------------->  TypeError: null is not a function
+// let str=null(num)
+// console.log(str)
+
+//undefined -------------->  TypeError: undefined is not a function
+// let str=undefined(num)
+// console.log(str)
+
+
+/***************************** if variable is bool, Null , undefined***************************/
+
+// let variable=null  
+// let variable=undefined
+// console.log(typeof variable)
+
+// let result=String(variable)
+// console.log(typeof result)
+// console.log(result)
+
+// let str="false"
+// bol=Boolean(str)
+// console.log(typeof str)
+// console.log(typeof bol)
+// console.log(bol)
+
+
+/*******************           operations              ******************/
+
+// console.log(3**3);
+// console.log(1/3);
+// console.log(4%3);
+
+//operations on string...
+
+// let str1="buttan"
+// let str2="Sarkar"
+// console.log(str1+str2)
+
+// console.log("2"+3)
+// console.log("2"+"3"+"4")
+// console.log(1+5+ "6")  //pahle num wale ko treat karke string me add kar dega
+// console.log(-"1")
+// console.log(-false)
+// console.log(+null)
+// console.log(+undefined)
+// console.log("*"+"2")
+
+// let incre=4
+// let result1=++incre
+// let result2=incre++
+
+// console.log(result1)
+// console.log(result2)
+// console.log(incre)
+
+
+
+
+
+
