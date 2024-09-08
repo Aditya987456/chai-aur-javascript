42d0285e0c1e8ff035d045f12c78d747ce854bef
ADITYA RAJ <132116417+Aditya987456@users.noreply.github.com>
2024-09-08 17:34:33 +0000

    comparison

---
+++ basic1/compare.js
@@ -0,0 +1,10 @@
+// >= and equility check both are different things in js
+
+// console.log("02">1)
+// console.log("2"==3)
+
+console.log(null==0)
+console.log(null > 0)   //comparison convert null to 0
+console.log(null>=0)    //comparison convert null to 0.
+
+
