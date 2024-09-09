e4742358834c87b81eca567a1071366c36920a62
ADITYA RAJ <132116417+Aditya987456@users.noreply.github.com>
2024-09-09 05:47:15 +0000

    comparison

--- basic1/compare.js
+++ basic1/compare.js
@@ -3,8 +3,17 @@
 // console.log("02">1)
 // console.log("2"==3)
 
-console.log(null==0)
-console.log(null > 0)   //comparison convert null to 0
-console.log(null>=0)    //comparison convert null to 0.
+// console.log(null==0)
+// console.log(null > 0)   comparison convert null to 0
+// console.log(null>=0)    comparison convert null to 0.
+
+
+//undefined always gives false but with ==null it gives true ...
+// console.log(undefined==null)
+// console.log(undefined>=null)
+// console.log(undefined<=null)
+
+console.log("2"==2)  //true
+console.log("2"===2)  //false becz data type different hai
 
 
