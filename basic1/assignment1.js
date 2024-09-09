238b29b198c8e2c0df04cd48d2b0dab5c342bc33
ADITYA RAJ <132116417+Aditya987456@users.noreply.github.com>
2024-09-09 06:31:07 +0000

    datatypes and its typeof

---
+++ basic1/assignment1.js
@@ -0,0 +1,33 @@
+/***************************  datatypes and its typeof  *********************/
+ 
+
+//primitive data types............
+
+let score=100
+const bigInt=78945612312345679n
+const str="aman"
+const bool=true
+const nulll=null
+let unde
+
+console.log(typeof score)  //number
+console.log(typeof bigInt) //undefined
+console.log(typeof str)     //string
+console.log(typeof bool)        //boolean
+console.log(typeof nulll)   //object
+console.log(typeof unde)    //undefined
+
+let arr=["buttan", "sarkar", "aman"]
+
+let myfunction=function(){
+    console.log("hellooooooooooooo")
+}
+
+let objectmera={
+    name:"buttan sarkar",
+    roll_no:12,
+}
+
+console.log(typeof myfunction)  //function(object)
+console.log(typeof objectmera)      //object
+console.log(typeof arr)     //object
