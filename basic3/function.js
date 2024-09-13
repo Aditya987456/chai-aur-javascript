13bdb07cecc500d32415ec4cb1af31e8525e22e6
ADITYA RAJ <132116417+Aditya987456@users.noreply.github.com>
2024-09-13 17:41:28 +0000

    function basic

---
+++ basic3/function.js
@@ -0,0 +1,65 @@
+//sum function
+
+// function sum(num1,num2){
+//  return num1+num2
+//     // console.log(num1+num2)
+// }
+
+
+// let num1=2
+// let num2=5
+
+// //result=sum(num1+num2) // this is wrong when console.log is there in the function
+// // sum(num1,num2)         //this is correct method
+
+// ans=sum(num1,num2)
+// console.log(ans)
+
+
+//***************** login user func */
+
+// function user_login(username = "sam"){  //ab kam se kam "sam" itna to naam dena padega khali string ka problem khatam...
+
+//     if(!username){
+//         console.log("invalid, please enter user name")
+//         return
+//     }
+
+//     return `${username} is just loggedIn`
+   
+//}
+
+//console.log(user_login("k"))         //calling
+
+// let result=Boolean()  //kab kab true or false hote hue jayega wo hai yeee.
+// console.log(result)
+
+
+
+
+
+
+
+
+
+// ****************shopping cart
+
+//function shopping_cart(...num){
+function shopping_cart(val1,val2, ...num){ //pahla two value argument ka val1 and val2 me jayega
+
+    return num
+}
+
+// const arr=(shopping_cart(100,200,300,400))
+// console.log(typeof arr)
+// console.log(arr)
+
+console.log(shopping_cart(100,200,456,44456))
+
+
+/*******************       Handling object in function     **************/
+
+
+
+
+
