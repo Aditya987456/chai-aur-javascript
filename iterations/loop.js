9ace4ce3aaafdf0a277ef956260bb616b9f9e733
ADITYA RAJ <132116417+Aditya987456@users.noreply.github.com>
2024-09-20 16:44:39 +0530

    Update loop.js

--- iterations/loop.js
+++ iterations/loop.js
@@ -1,15 +1,31 @@
 //for loop 1-10 
 //ctrl+ d duplicate selected in codes
 
-//node "c:\Users\HP\OneDrive\Desktop\codeforpush\promisified.js"
+
 
 //for loop...
 //table from 1 to 20
 
 for(let i=1; i<=20; i++){
+
+
+//continue............................................
+    if(i==5){
+        console.log(`5 ke table ko hi skip karna tha aage badho.\n`)
+        continue;
+    }
+
+
     for(let j=1; j<=10; j++){
         console.log(`${i} * ${j} = ${i*j}`)
     }
     console.log("\n")
-    // console.log(i)
+
+
+//break............................................................
+    // if(i==5){
+    //     console.log(`5 ke table tak hi print karna tha ab stop.`)
+    //     break;
+    // }
+
 }
