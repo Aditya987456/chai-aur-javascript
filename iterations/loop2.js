b593b42dcb673ac12c4f25b6b3a28246f32d7628
ADITYA RAJ <132116417+Aditya987456@users.noreply.github.com>
2024-09-20 17:00:09 +0530

    Update loop2.js

---
+++ iterations/loop2.js
@@ -0,0 +1,21 @@
+//while loop................
+
+
+let index=0;
+while(index<=10){
+    console.log(`${index}`)
+    index++
+}
+
+
+
+//do-while...................
+
+
+console.log("\n")
+let Score =0
+do{
+    console.log(`score is ${Score}`)
+    Score++
+   
+}while (Score !=4);
