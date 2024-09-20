39d82a72c968aab93f8c086ea3769b4118bf6643
ADITYA RAJ <132116417+Aditya987456@users.noreply.github.com>
2024-09-20 16:34:07 +0530

    Add files via upload

---
+++ iterations/loop.js
@@ -0,0 +1,15 @@
+//for loop 1-10 
+//ctrl+ d duplicate selected in codes
+
+//node "c:\Users\HP\OneDrive\Desktop\codeforpush\promisified.js"
+
+//for loop...
+//table from 1 to 20
+
+for(let i=1; i<=20; i++){
+    for(let j=1; j<=10; j++){
+        console.log(`${i} * ${j} = ${i*j}`)
+    }
+    console.log("\n")
+    // console.log(i)
+}
